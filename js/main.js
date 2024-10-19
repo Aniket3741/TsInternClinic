cb091a94e4397503b21c521f6de3f6128e41b165
Aniket kumar <mailtoaniketkumar2021@gmail.com>
2024-10-19 12:53:39 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// HEADER SCROLL
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 100) {
+        nav.classList.add("header-scrolled");
+    }
+    else {
+        nav.classList.remove("header-scrolled");
+    }
+}
+// NAV HIDE
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
+        navCollapse.classList.remove("show");
+    })
+})
